8c0d9957e4ea27d107634834191f04d13b308932
smalam119 <smalam119@gmail.com>
2019-03-02 01:28:07 +0600

    precision, recall and f1 added for single label

--- ClassificationMetrics.py
+++ ClassificationMetrics.py
@@ -11,14 +11,23 @@ class ClassificationMetrics:
     def calculate_accuracy(self):
         return
 
-    def calculate_precision(self):
-        return
+    def calculate_precision(self, confusion_matrix):
+        true_positive = confusion_matrix[0][0]
+        false_positive = confusion_matrix[1][0]
+        precision = true_positive / (true_positive + false_positive)
+        return precision
 
-    def calculate_recall(self):
-        return
+    def calculate_recall(self, confusion_matrix):
+        true_positive = confusion_matrix[0][0]
+        false_negative = confusion_matrix[0][1]
+        recall = true_positive / (true_positive + false_negative)
+        return recall
 
-    def calculate_f1(self):
-        return
+    def calculate_f1(self, confusion_matrix):
+        precision = self.calculate_precision(confusion_matrix)
+        recall = self.get_recall(confusion_matrix)
+        f1_score = 2 * (precision * recall) / (precision + recall)
+        return f1_score
 
     def calculate_roc_values(self):
         return
