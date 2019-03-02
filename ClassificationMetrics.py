adb67de17c6c00c17027d8007141aa82d59f5934
smalam119 <smalam119@gmail.com>
2019-03-03 01:17:42 +0600

    get_specificity,classify images, classification metric init added

--- ClassificationMetrics.py
+++ ClassificationMetrics.py
@@ -1,9 +1,27 @@
+import numpy as np
+
+
 class ClassificationMetrics:
 
-    def __init__(self, training_labels, predicted_labels, predicted_scores):
+    confusion_matrix = np.zeros((2, 2), np.int32)
+
+    def __init__(self, label, training_labels, predicted_labels, predicted_scores, file_path):
         self.training_labels = training_labels
         self.predicted_labels = predicted_labels
         self.predicted_scores = predicted_scores
+
+        for x in range(len(training_labels)):
+            if training_labels[x] == label and predicted_labels[x] == label:
+                self.confusion_matrix[0][0] = self.confusion_matrix[0][0] + 1
+            elif training_labels[x] == label and predicted_labels[x] != label:
+                self.confusion_matrix[1][0] = self.confusion_matrix[1][0] + 1
+            elif training_labels[x] != label and predicted_labels[x] == label:
+                self.confusion_matrix[0][1] = self.confusion_matrix[0][1] + 1
+            elif training_labels[x] != label and predicted_labels[x] != label:
+                self.confusion_matrix[1][1] = self.confusion_matrix[1][1] + 1
+
+        np.savetxt(file_path + "/confusion_matrix.txt", self.confusion_matrix)
+
 
     def get_confusion_matrix_for_heat_map(self):
         return
@@ -11,23 +29,29 @@ class ClassificationMetrics:
     def calculate_accuracy(self):
         return
 
-    def calculate_precision(self, confusion_matrix):
-        true_positive = confusion_matrix[0][0]
-        false_positive = confusion_matrix[1][0]
+    def calculate_precision(self):
+        true_positive = self.confusion_matrix[0][0]
+        false_positive = self.confusion_matrix[1][0]
         precision = true_positive / (true_positive + false_positive)
         return precision
 
-    def calculate_recall(self, confusion_matrix):
-        true_positive = confusion_matrix[0][0]
-        false_negative = confusion_matrix[0][1]
+    def calculate_recall(self):
+        true_positive = self.confusion_matrix[0][0]
+        false_negative = self.confusion_matrix[0][1]
         recall = true_positive / (true_positive + false_negative)
         return recall
 
-    def calculate_f1(self, confusion_matrix):
-        precision = self.calculate_precision(confusion_matrix)
-        recall = self.get_recall(confusion_matrix)
+    def calculate_f1(self):
+        precision = self.calculate_precision(self.confusion_matrix)
+        recall = self.get_recall(self.confusion_matrix)
         f1_score = 2 * (precision * recall) / (precision + recall)
         return f1_score
+
+    def get_specificity(self, confusion_matrix):
+        true_negative = confusion_matrix[1][1]
+        false_positive = self.confusion_matrix[1][0]
+        specificity = true_negative / (true_negative + false_positive)
+        return specificity
 
     def calculate_roc_values(self):
         return
--- NaiveClassifier.py
+++ NaiveClassifier.py
@@ -1,4 +1,5 @@
-
+import sys
+import numpy as np
 
 class NaiveClassifier:
 
@@ -13,8 +14,36 @@ class NaiveClassifier:
     def extract_feature_from_multiple_images(self):
         return
 
-    def classify_single_image(self):
-        return
+    def classify_single_image(self, test_image):
+        smallest_distance = sys.maxsize
+        for x in range(len(self.training_images)):
+            dist = self.calculate_euclidean_distance(self, test_image, self.training_images[x])
+            if dist < smallest_distance:
+                smallest_distance = dist
+                predicted_label = self.training_label[x]
+            if smallest_distance == 0:
+                break
+            else:
+                continue
+            break
+        return predicted_label
 
-    def classify_multiple_images(self):
-        return
+    def classify_multiple_images(self, test_images):
+        smallest_distance = sys.maxsize
+        predicted_label_list = []
+        for x in range(len(test_images)):
+            for y in range(len(self.training_images)):
+                dist = self.calculate_euclidean_distance(self, test_images[x], self.training_images[y])
+                if dist < smallest_distance:
+                    smallest_distance = dist
+                    predicted_label = self.training_label[y]
+                    predicted_label_list.append(predicted_label)
+                if smallest_distance == 0:
+                    break
+                else:
+                    continue
+                break
+        return predicted_label_list
+
+    def calculate_euclidean_distance(self, x, y):
+        return np.sqrt(np.sum((x - y) ** 2))
